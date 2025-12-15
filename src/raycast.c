e9805207b07c946c59a120bd54fb5e9b626db8ed
Edgar Amorim <edgarr.amorim@gmail.com>
2025-12-15 17:39:40 +0000

    Add door rendering

--- src/raycast.c
+++ src/raycast.c
@@ -6,6 +6,8 @@
 #include <math.h>
 
 static void	fill_rayhelper(struct s_rayhelper *helper, t_vec2f pos, t_vec2f dir);
+static void	render_ray_step(struct s_rayhelper *info, t_rayhit *hit);
+static int	render_ray_check(t_scene *scene, struct s_rayhelper *info, t_rayhit *hit);
 
 //TODO: make a limited distance version
 //TODO: make one for sprite rendering (needs to return multiple results)
@@ -17,30 +19,8 @@ t_rayhit	cast_render_ray(t_scene *const scene, const t_vec2f pos, const t_vec2f 
 	struct s_rayhelper	info;
 
 	fill_rayhelper(&info, pos, dir);
-	hit.hit = 0;
-	while (!hit.hit)
-	{
-		if (info.length.x < info.length.y)
-		{
-			info.length.x += info.length_step.x;
-			info.map_pos.x += info.map_step.x;
-			if (info.map_step.x < 0)
-				hit.side_hit = EAST;
-			else
-				hit.side_hit = WEST;
-		}
-		else
-		{
-			info.length.y += info.length_step.y;
-			info.map_pos.y += info.map_step.y;
-			if (info.map_step.y < 0)
-				hit.side_hit = NORTH;
-			else
-				hit.side_hit = SOUTH;
-		}
-		if (scene->map[info.map_pos.x][info.map_pos.y] == WALL)
-			hit.hit = 1;
-	}
+	while (!render_ray_check(scene, &info, &hit))
+		render_ray_step(&info, &hit);
 	if (hit.side_hit & 0b01)
 	{
 		hit.projDist = info.length.x - info.length_step.x;
@@ -52,6 +32,8 @@ t_rayhit	cast_render_ray(t_scene *const scene, const t_vec2f pos, const t_vec2f 
 		hit.hit_position.x = pos.x + hit.projDist * dir.x;
 	}
 	hit.hit_position.x = hit.hit_position.x - floorf(hit.hit_position.x);
+	if (hit.side_hit >> 2)
+		hit.side_hit = DOOR_TEX;
 	return (hit);
 }
 
@@ -80,4 +62,49 @@ static void	fill_rayhelper(struct s_rayhelper *const helper,
 		helper->map_step.y = 1;
 		helper->length.y = (helper->map_pos.y + 1 - pos.y) * helper->length_step.y;
 	}
+}
+
+static void	render_ray_step(struct s_rayhelper *info, t_rayhit *hit)
+{
+	if (info->length.x < info->length.y)
+	{
+		info->length.x += info->length_step.x;
+		info->map_pos.x += info->map_step.x;
+		if (info->map_step.x < 0)
+			hit->side_hit = EAST;
+		else
+			hit->side_hit = WEST;
+	}
+	else
+	{
+		info->length.y += info->length_step.y;
+		info->map_pos.y += info->map_step.y;
+		if (info->map_step.y < 0)
+			hit->side_hit = NORTH;
+		else
+			hit->side_hit = SOUTH;
+	}
+}
+
+static int	render_ray_check(t_scene *scene, struct s_rayhelper *info, t_rayhit *hit)
+{
+	t_entity	*entity;
+
+	if (scene->map[info->map_pos.x][info->map_pos.y] == 0)
+		return (0);
+	if (scene->map[info->map_pos.x][info->map_pos.y] == WALL)
+		return (1);
+	entity = scene->entities + (scene->map[info->map_pos.x][info->map_pos.y] >> 2);
+	if (entity->type == DOOR && !entity->open)
+	{
+		if (!(hit->side_hit & 0b01) && !(info->length.y - info->length_step.y + info->length_step.y/4 < info->length.x))
+			return (0);
+		if (hit->side_hit & 0b01 && !(info->length.x - info->length_step.x + info->length_step.x/4 < info->length.y))
+			return (0);
+		hit->side_hit |= 0b100;
+		info->length.x += info->length_step.x / 4;
+		info->length.y += info->length_step.y / 4;
+		return (1);
+	}
+	return (0);
 }
